a7dc897ee4b69f442cc89d42e72d9a49aa0eb3a3
Softwarica College Lab <softwarica@softwarica.com>
2019-04-19 08:23:36 +0545

    Recycler view to description

--- app/src/main/java/adapter/ContactsAdapter.java
+++ app/src/main/java/adapter/ContactsAdapter.java
@@ -1,12 +1,15 @@
 package adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.recyclerview.DetailsActivity;
 import com.example.recyclerview.R;
 
 import java.util.List;
@@ -23,21 +26,36 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.Contac
         this.contactsList = contactsList;
     }
 
-
     @NonNull
     @Override
     public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext ()).inflate ( R.layout.contact,viewGroup,false );
+        return new ContactsViewHolder ( view );
     }
 
     @Override
     public void onBindViewHolder(@NonNull ContactsViewHolder contactsViewHolder, int i) {
+       final Contacts contacts = contactsList.get ( i );
 
+        contactsViewHolder.imgProfile.setOnClickListener ( new View.OnClickListener (){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent ( mContext, DetailsActivity.class );
+
+                intent.putExtra ( "image",contacts.getImageId () );
+                intent.putExtra ( "image",contacts.getName() );
+                intent.putExtra ( "image",contacts.getPhoneNo () );
+            }
+        } );
+
+        contactsViewHolder.imgProfile.setImageResource ( contacts.getImageId () );
+        contactsViewHolder.tvName.setText ( contacts.getName () );
+        contactsViewHolder.tvPhone.setText ( contacts.getPhoneNo () );
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contactsList.size ();
     }
 
     public class ContactsViewHolder extends RecyclerView.ViewHolder{
--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.ContactsAdapter;
+import model.Contacts;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById ( R.id.recycler );
+        List<Contacts> contactsList = new ArrayList<> (  );
+        contactsList.add ( new Contacts ( "Saugat Malla","90800011264" , R.drawable.saugat ) );
+        contactsList.add ( new Contacts ( "Rajesh Hamal","98320011264", R.drawable.rajesh ) );
+        contactsList.add ( new Contacts ( "Dahayang Rai","9800716544", R.drawable.dahayang ) );
+        contactsList.add ( new Contacts ( "Bhuwan KC","9086532833", R.drawable.bhuwan ) );
+
+        ContactsAdapter contactsAdapter = new ContactsAdapter ( this,contactsList );
+        recyclerView.setAdapter ( contactsAdapter );
+        recyclerView.setLayoutManager ( new LinearLayoutManager ( this, LinearLayoutManager.VERTICAL, false ) );
+
     }
 }
